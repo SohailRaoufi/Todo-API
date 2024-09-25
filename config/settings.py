9328457991d5b3500752e155b831d1bd76a6de9f
Ahmad Sohail Raoufi <sohailraufi19@gmail.com>
2024-09-25 20:12:27 +0430

    Updated the Setting

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#08s+dra8#eh2dn$*7r8xoaxz$4d#yy!pw3)ms7u&)8*zyrlju'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
 
@@ -64,6 +65,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = (
     "http://localhost:3000",
     "http://localhost:8000",
+    "http://127.0.0.1:8000"
 )
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
